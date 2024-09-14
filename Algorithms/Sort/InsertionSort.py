ae135c9cb3d887ce662d91e45eddd6d7fdcde67a
bluergost <kevinstephenbiswas@gmail.com>
2024-09-14 17:31:47 +0900

    Setup project structure

---
+++ Algorithms/Sort/InsertionSort.py
@@ -0,0 +1,6 @@
+class InsertionSort:
+    def __init__(self):
+        pass
+
+    def Sort(self, nums:list[float]) -> list[float]:
+        print("insertion Sort")
---
+++ Algorithms/Sort/__init__.py
@@ -0,0 +1,1 @@
+from .InsertionSort import InsertionSort
