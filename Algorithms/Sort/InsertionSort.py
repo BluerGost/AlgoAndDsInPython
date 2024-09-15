5e2e499c098c3dee1c43a518b7a3ef00593977d0
bluergost <kevinstephenbiswas@gmail.com>
2024-09-15 18:46:11 +0900

    Insertion sort working. Test case creation remaining.

--- Algorithms/Sort/InsertionSort.py
+++ Algorithms/Sort/InsertionSort.py
@@ -3,4 +3,18 @@ class InsertionSort:
         pass
 
     def Sort(self, nums:list[float]) -> list[float]:
-        print("insertion Sort")
+        numsLen = len(nums)
+
+        for currIndex in range(1, numsLen):
+            prevIndex = currIndex - 1
+            currValue = nums[currIndex]
+
+            while prevIndex >= 0 and nums[prevIndex] > currValue:
+                nums[prevIndex + 1] = nums[prevIndex]
+                prevIndex -= 1
+
+            nums[prevIndex + 1] = currValue
+
+
+
+
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import Algorithms
 
 def main():
-    insertionSort = Algorithms.InsertionSort()
-    insertionSort.Sort([])
+
+    # # Insersion Sort
+    # insertionSort = Algorithms.InsertionSort()
+    # testList = [5, 6 ,1, 4, -1, -3, 6, 10]
+    # insertionSort.Sort(testList)
+    # print(testList)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
